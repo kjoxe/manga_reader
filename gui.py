fa638f031775754d1d95f54126bc8047eb1bf65d
kjoxe <marmitee@protonmail.com>
2017-12-16 14:43:36 +0000

    Basic program skeleton implemented. Can load images from file based on shell args and can scroll through the different images using j&space to go to the next and k to go back. Currently bad layout

--- gui.py
+++ gui.py
@@ -1,4 +1,3 @@
-## TODO: Add zip loading support
 ## TODO: Add KEYBINDS! j/down/space=next k/up=prev q=quit
 ## TODO: Split picture up and display half on either side of screen
 ## TODO: Zooming??
@@ -6,19 +5,24 @@
 import sys
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QScrollArea
-from PyQt5.QtGui import QIcon, QPixmap, QPalette
+from PyQt5.QtGui import QIcon, QPixmap, QPalette, QImage
 
-import time
+import mangazip
+
+class Image_Viewer(QWidget):
  
-class App(QWidget):
- 
-    def __init__(self):
+    def __init__(self, myzip, imagepaths):
         super().__init__()
         self.title = 'Manga Reader'
         self.left = 10
         self.top = 10
         self.width = 640
         self.height = 480
+
+        self.zip=myzip
+        self.imagepaths=imagepaths
+        self.current_image_index=0
+
         self.initUI()
  
     def initUI(self):
@@ -30,25 +34,42 @@ class App(QWidget):
         self.scrollArea.setWidgetResizable(True)
         self.label = QLabel(self.scrollArea)
         self.label.setScaledContents(True)
- 
+
+        self.updateUI(self.imagepaths[self.current_image_index])
+
         self.show()
 
-    def updateUI(self, image):
-        pixmap = QPixmap(image)
-        self.label.setPixmap(pixmap)
-        self.resize(pixmap.width(),pixmap.height())
-        self.scrollArea.resize(self.size())
-        self.label.resize(self.size())
+    def updateUI(self, imagepath):
+        print(imagepath)
+        with self.zip.open(imagepath) as f:
+            image=f.read()
+            pixmap = QPixmap.fromImage(QImage.fromData(image))
+            self.label.setPixmap(pixmap)
+            self.resize(pixmap.width(),pixmap.height())
+            self.scrollArea.resize(self.size())
+            self.label.resize(self.size())
+
+    def nextImage(self):
+        if self.current_image_index<len(self.imagepaths)-1:
+            self.current_image_index+=1
+        self.updateUI(self.imagepaths[self.current_image_index])
+
+    def prevImage(self):
+        if self.current_image_index>0:
+            self.current_image_index-=1
+        self.updateUI(self.imagepaths[self.current_image_index])
 
     def keyPressEvent(self, e):
         if e.key() == Qt.Key_Q:
             self.close()
 
         elif e.key() == Qt.Key_J or e.key() == Qt.Key_Space:
-            self.updateUI(Loader.getNextImage())
+            self.nextImage()
+        elif e.key() == Qt.Key_K:
+            self.prevImage()
  
-if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    ex = App()
-    ex.updateUI('vohiyo.jpg')
-    sys.exit(app.exec_())
+class Manga_Gui():
+    def __init__(self, myzip, imagepaths):
+        app = QApplication(sys.argv)
+        self.viewer = Image_Viewer(myzip, imagepaths)
+        sys.exit(app.exec_())
---
+++ run.py
@@ -0,0 +1,36 @@
+import sys
+import zipfile
+
+import mangazip
+import gui
+
+def print_list(myList):
+    for value in myList:
+        print(value)
+
+if len(sys.argv)<=2: # no zip file to open
+    print("Manga Reader help:")
+    sys.exit()
+if not zipfile.is_zipfile(sys.argv[2]):
+    print(sys.argv[0] + ": Invalid zip file.")
+    sys.exit()
+
+with zipfile.ZipFile(sys.argv[2]) as myzip:
+    if len(sys.argv)==3: # No internal directory passed in
+        internal_directory=""
+        dir_depth=0
+    else:
+        internal_directory=sys.argv[3]
+        dir_depth=mangazip.file_depth(internal_directory)+1 # +1 lets us look inside of the directory
+    if sys.argv[1]=="list":
+        dirs=mangazip.get_files_at_path(myzip, True, dir_depth, internal_directory)
+        print_list(dirs)
+        files=mangazip.get_files_at_path(myzip, False, dir_depth, internal_directory)
+        print_list(files)
+        sys.exit()
+    elif sys.argv[1]=="open":
+        imagepaths=mangazip.get_files_at_path(myzip, False, dir_depth, internal_directory) # TODO: Add check for image files rather than just if files exist
+        if len(imagepaths)==0:
+            print(sys.argv[0] + ": No images at that location.")
+            sys.exit()
+        manga_gui = gui.Manga_Gui(myzip, imagepaths)
