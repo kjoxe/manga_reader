b2ead89840f440b9e0d3b90d47a3711bef604633
kjoxe <marmitee@protonmail.com>
2017-12-17 15:47:21 +0000

    Simplified command line ops as they were mostly points. Changed help and added to README.md as well

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # manga_reader
+
+Very minimal raw manga reader for my personal use.
+
+## Basic usage
+
+$ manga_reader zipfile
+
+  Next image - j/space
+  Prev image - k
+  Quit       - q
--- manga_reader/mangahelp.py
+++ manga_reader/mangahelp.py
@@ -1,20 +1,14 @@
 name="Manga Reader"
 version="0.1a"
 def print_help(command):
-    help_message="""
-__NAME__ __VERSION__
+    help_message="""__NAME__ __VERSION__
 
-  usage: __COMMAND__ open path_to_zip
-     or: __COMMAND__ open-dir/list path_to_zip [internal_folder]
+usage: __COMMAND__ zipfile
 
-options:
-
-  open     - Open all files in GUI and scroll through in alphabetical order
-  open-dir - Only open the files contained in the specified directory.
-             If no directory specified then only the root directory will be opened
-  list     - List files contained in the specified directory.
-             If no directory specified then only the root directory will be printed"""
+  Next image - j/space
+  Prev image - k
+  Quit       - q"""
     help_message=help_message.replace("__NAME__", name)
     help_message=help_message.replace("__VERSION__", version)
-    help_message=help_message.replace("__COMMAND__", command)
+    help_message=help_message.replace("__COMMAND__", command.split("/")[-1])
     print(help_message)
